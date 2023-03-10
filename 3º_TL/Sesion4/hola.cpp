211c5592262d1d09ed6948d830257e0886acbf4d
DeltΔ <91746122+Deluwuta@users.noreply.github.com>
2023-03-10 20:47:20 +0100

    Add files via upload

---
+++ 3º_TL/Sesion4/hola.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  vector<string> vec = {"Enero", "Febrero"};
+
+  cout << vec[0] << " uwu " << vec[1] << endl;
+
+  return 0;
+}
